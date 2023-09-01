fe91dd2a7188a8369b2470ca8dcc159ca23b6259
MS shamim <141145885+shamim2821@users.noreply.github.com>
2023-09-01 18:29:30 +0000

    script file cleared

--- scripts/script.js
+++ scripts/script.js
@@ -3,37 +3,3 @@
 
 let quantityvalue = document.querySelectorAll('.option');
 
-
-// All event listener
-addbutton.forEach(element => {
-    element.addEventListener('click', addproduct);
-});
-
-
-
-// All functions 
-let cart_quantity = 0;
-function addproduct(){
-
-
- 
-    // quantityvalue.forEach((e) => {
-
-    //     console.log(e.id);
-
-    //     if (e.value == ''){
-    //         cart_quantity = cart_quantity + 1;
-    //     }else{
-    //         cart_quantity = cart_quantity + Number(e.value);
-    //         e.value = '';
-    //     }
-    // })
-
-
-    
-     
-    
-
-    
-
-}
